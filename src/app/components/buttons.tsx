3b6d5b8f7a466494355d931a8b015405cc0959ca
Andy Johnson <andyjohnson70@gmail.com>
2024-03-13 13:49:49 -0700

    Setting up postgres DB

---
+++ src/app/components/buttons.tsx
@@ -0,0 +1,26 @@
+
+export function XpButton() {
+    return (
+        <div className="bg-sky-600 h-1/2 m-2">
+            <div>
+                <div>Buy XP</div>
+                <div>
+                    <div>4</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function RerollButton() {
+    return (
+        <div className="bg-yellow-400  h-1/2 m-2">
+            <div>
+                <div>Reroll</div>
+                <div>
+                    <div>2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
